71e3126b0cbb9fd446ecc41794b50362d385633a
S-Ebadi <said.ebadi@chasacademy.se>
2025-09-26 22:16:35 +0200

    updated

--- Psutilbonusfördennyfikna.py
+++ Psutilbonusfördennyfikna.py
@@ -1,5 +1,4 @@
 Kolla andra delen av readme-filen.
 
-OM det fortfarande skulle råda frågetecken- 
-eller ifall du bara vill spara och utöka-
-eget bibliotek.
+OM det fortfarande skulle råda frågetecken eller 
+ifall du bara vill spara och utökeget bibliotek.
